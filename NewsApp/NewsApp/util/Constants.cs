1f86054d6328d47210a184b0d613facb22dd47d6
Sasha <TOT@DESKTOP-742U3GF>
2018-10-16 08:00:37 -0700

    added new classes

---
+++ NewsApp/NewsApp/util/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsApp.util
+{
+    class Constants
+    {
+        private static Constants instance;
+        private Constants() { }
+
+        public static Constants getInstance()
+        {
+            if (instance == null)
+                instance = new Constants();
+            return instance;
+        }
+
+        public string url = "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=f74ef05b6a3548c19183980c5a90c444";
+    }
+}
